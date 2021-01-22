69436873f7116009003fbbd9153e3b37442af71b
rj0-dev <joelisata@topteam.tech>
2021-01-22 09:51:02 +0300

    Fixing issue with installing font

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 const { exec, execSync } = require("child_process");
 const fs = require("fs");
 const https = require("https");
+const path = require("path");
 
 const FONT_DIRECTORY = "C:\\windows\\fonts\\";
-const FONTS_KEY =
-  "HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Fonts";
+const FONTS_KEY = "HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Fonts";
 
 module.exports = async (job, settings, { fonts }) => {
   if (!fonts.length) return "No Fonts Supplied";
 
   const promises = fonts.map(async ({ src, name }) => {
-    await getFileFromUrl(src);
     const fileName = src.split("/").pop();
 
+    let filePath = null;
     try {
       execSync(`REG QUERY "${FONTS_KEY}" /v "${name}"`);
       return "Already Installed";
     } catch (err) {
       try {
-        execSync(
-          `REG ADD "${FONTS_KEY}" /v "${name}" /t REG_SZ /d ${fileName} /f`
-        );
+        const res = await getFileFromUrl(src);
+        filePath = res.filePath;
+        execSync(`COPY /Y "${filePath}" "${FONT_DIRECTORY}"`);
+        execSync(`REG ADD "${FONTS_KEY}" /v "${name}" /t REG_SZ /d "${fileName}" /f`);
         return "Installed";
       } catch (err) {
         throw new Error(err);
+      } finally {
+        if (!filePath) return;
+        execSync(`ERASE /F "${filePath}"`);
       }
     }
   });
@@ -33,7 +37,7 @@ module.exports = async (job, settings, { fonts }) => {
 const getFileFromUrl = (src) => {
   return new Promise((resolve, reject) => {
     const fileName = src.split("/").pop();
-    const filePath = `${FONT_DIRECTORY}${fileName}`;
+    const filePath = path.join(process.env.TMP, fileName);
     const file = fs.createWriteStream(filePath);
 
     https.get(src, (response) => {
